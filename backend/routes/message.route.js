f215eb467f94af6766d1269b262eb48eba909230
Arnav Sawant <arnavsawant.as@gmail.com>
2024-12-24 01:54:27 +0530

    setting up frontend

--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUsersForSidebar } from '../controllers/message.controller.js';
+import { getMessages, getUsersForSidebar, sendMessage } from '../controllers/message.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
