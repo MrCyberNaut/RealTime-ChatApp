53e61025deb53d234a8681dee521010d4db07bec
Arnav Sawant <arnavsawant.as@gmail.com>
2024-12-31 02:46:50 +0530

    signup page frontend

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import {create} from "zustand";
 import{ axiosInstance} from "../lib/axios.js";
+import { toast } from "react-hot-toast";
 
 
 export const useAuthStore = create((set) => ({ // takes the first argument as a callback function that receives a set function
@@ -24,6 +25,31 @@ export const useAuthStore = create((set) => ({ // takes the first argument as a 
         finally{
             set({isCheckingAuth : false}); // sets the state of the store
         }
-    }
+    },
 
+    signup : async(data) => {
+        set({isSigningUp : true});
+            try {
+             const res =   await axiosInstance.post("/auth/signup",data); //passing the data that the user sends us to the backend
+             
+             set({authUser : res.data}); // sets the state of the store
+             toast.success("Account created successfully");
+            } catch (error) {
+                toast.error(error.response.data.message);//grabbing the message sent from the signup
+                    console.log("Error in signup",error);
+            }
+
+            finally{
+                set({isSigningUp : false});
+            }
+    },
+    logout : async() => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({authUser : null}); // sets the state of the store
+            toast.success("Logged out successfully");
+        } catch (error) {
+            console.log("Error in logout",error);
+        }
+    }
 }));
