70cf45d6e8fb157b4eff9104f8440306f951f189
Arnav Sawant <arnavsawant.as@gmail.com>
2025-01-03 01:06:27 +0530

    themes added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,4 +10,7 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
+  daisyui:{
+    themes:["light", "dark","cupcake","retro","bumblebee","emerald","corporate","synthwave","cyberpunk","valentine","halloween","garden","forest","aqua","lofi","pastel","fantasy","wireframe","black","luxury","dracula"] ,
+  }
 }
