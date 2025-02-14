ed9d18184db5f69e0d5101f56e3dee81b807b4c8
Arnav Sawant <arnavsawant.as@gmail.com>
2025-02-14 23:39:18 +0530

    socket js configuring

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -18,10 +18,10 @@ io.on("connection", (socket) => {
     console.log("a user connected", socket.id);
 
     const userId = socket.handshake.query.userId; //passing to client whichis the useAuthStire in frontend
-    if(userId) userSocketMap[userId] = socket.id;
+    if(userId) userSocketMap[userId] = socket.id;// if userid exsists update the socket map with the socket id
 
 
-    io.emit(); //emit is used to send events to all the connected clients
+    io.emit("getOnlineUsers",Object.keys(userSocketMap)); //emit is used to send events to all the connected clients
 
     socket.on("disconnect", () => {
         console.log("user disconnected", socket.id);
